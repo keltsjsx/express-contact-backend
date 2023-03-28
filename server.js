80304be5cf94c1032600cfbf24a506d99567f3d2
keltsjsx <kelts.jsx@gmail.com>
2023-03-28 13:51:41 +0700

    first endpoint and thunder client setup

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 
 const port = process.env.PORT || 5000
 
+app.get('/api/contact', (req, res) => {
+  res.status(200).json({
+    message: "Get all contacts"
+  })
+})
+
 app.listen(port, () => {
   console.log(`this server running on port ${port}`);
 })
